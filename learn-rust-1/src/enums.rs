57a2ed663f3900340fdbe90e6a433b0b119ebab7
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2026-07-07 14:52:19 +0530

    Learn about enums, "if let", and "let else"

--- learn-rust-1/src/enums.rs
+++ learn-rust-1/src/enums.rs
@@ -24,4 +24,21 @@ fn main() {
     my_ip.print();
 
     other_ip.print();
+
+    if let IPVariant::V6(ref ip) = other_ip {
+        // This is the important part we want to run depending on whether it matches.
+        println!("This is version 6. IP - {ip:?}");
+    } else {
+        // This part runs if it doesn't match.
+        println!("This is version 4.");
+    }
+
+    let IPVariant::V4(f1, f2, _, _) = my_ip else {
+        println!("It is not a IPv4 address.");
+        return; // Stops executing.
+    };
+
+    // Now the fields f1, f2 are in the main scope. I can do whatever I want with them without going inside many curly braces.
+
+    println!("The first two fields of the IPv4 address are {f1} and {f2}.");
 }
