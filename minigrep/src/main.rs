9d1db2118ce7feead322e636e58048e0b01f9524
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2026-07-10 15:58:42 +0530

    Read environment variables

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use minigrep::search;
+use minigrep::search_case_insensitive;
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -21,6 +22,7 @@ fn main() {
 struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -33,15 +35,27 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
 
-    for line in search(&config.query, &contents) {
-        println!("{line}");
+    if config.ignore_case {
+        for line in search_case_insensitive(&config.query, &contents) {
+            println!("{line}");
+        }
+    } else {
+        for line in search(&config.query, &contents) {
+            println!("{line}");
+        }
     }
 
     Ok(())
