26b83d52237fa1624cdf861b940ab7f016cc651d
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2026-07-10 14:36:39 +0530

    Start creating a mini version of a tool similar to `grep`

---
+++ minigrep/src/main.rs
@@ -0,0 +1,48 @@
+use minigrep::search;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem with parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(&config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        // args[0] is the program name.
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
