b74939062e05e59aa960e59d01e2d7a4fb589115
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2026-07-10 16:34:01 +0530

    Take both env variable and cli arguments with cli having high precedence

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -35,13 +35,42 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        let ignore_case_env = env::var("IGNORE_CASE").is_ok();
+
+        let ignore_case_cli = is_cli_ignore_case_set(&args);
+
+        let ignore_case = decide_ignore_case(ignore_case_env, ignore_case_cli);
 
         Ok(Config {
             query,
             file_path,
             ignore_case,
         })
+    }
+}
+
+fn is_cli_ignore_case_set(args: &[String]) -> Option<bool> {
+    if args.len() < 4 {
+        return None;
+    }
+
+    let Some((prefix, suffix)) = args[3].split_once('=') else {
+        return None;
+    };
+
+    if prefix.eq("IGNORE_CASE") && suffix.eq("TRUE") {
+        return Some(true);
+    } else if prefix.eq("IGNORE_CASE") && suffix.eq("FALSE") {
+        return Some(false);
+    }
+
+    None
+}
+
+fn decide_ignore_case(env_set: bool, cli_set: Option<bool>) -> bool {
+    match cli_set {
+        Some(cli_set) => cli_set,
+        None => env_set,
     }
 }
 
