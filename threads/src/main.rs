f9a121596a2284764751b830906c3eb2bc47e79a
Linuka Ratnayake <79963204+linukaratnayake@users.noreply.github.com>
2026-07-15 14:49:04 +0530

    Experiment with threads

---
+++ threads/src/main.rs
@@ -0,0 +1,42 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            println!("Sending... {val}");
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("this"),
+            String::from("is"),
+            String::from("another"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            println!("Sending... {val}");
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
+}
